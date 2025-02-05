e15e5330ea11598c69e6c2a20961a029383e25cb
nikhilShakya7 <rsnikhil77@gmail.com>
2025-02-05 21:59:57 +0545

    Homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,26 +6,46 @@ const Home = () => {
   const navigate = useNavigate(); // Hook for navigation
 
   return (
-    <div className={styles["home-page"]}>
-      {" "}
-      {/* Use styles from CSS module */}
-      <div className={styles.container}>
-        <div className={styles["background-container"]}>
-          <h1>Tech</h1>
-          <h2>Nest</h2>
+    <>
+      <div className={styles["home-page"]}>
+        {" "}
+        {/* Use styles from CSS module */}
+        <div className={styles.container}>
+          <div className={styles["background-container"]}>
+            <h1>Tech</h1>
+            <h2>Nest</h2>
+          </div>
+          <div className={styles.text}>
+            <p>
+              At TechNest, we make shopping exciting, effortless, and rewarding
+              with quality products, seamless browsing, secure transactions, and
+              exceptional customer support.
+            </p>
+          </div>
         </div>
-        <div className={styles.text}>
-          <p>
-            At TechNest, we make shopping exciting, effortless, and rewarding
-            with quality products, seamless browsing, secure transactions, and
-            exceptional customer support.
-          </p>
+        <button className={styles.button} onClick={() => navigate("/shop")}>
+          Shop 🛒
+        </button>
+      </div>
+      <h3>Our Services</h3>
+      <div className={styles.galleryContainer}>
+        <div className={styles.galleryContainer}>
+          {[
+            { src: "./laptop.webp", alt: "Laptops" },
+            { src: "./mobile.webp", alt: "Mobiles" },
+            { src: "./tablet.jpg", alt: "Tablets" },
+            { src: "/img_mountains.jpg", alt: "Mountains" },
+          ].map((image, index) => (
+            <div key={index} className={styles.gallery}>
+              <a href={image.src} target="_blank" rel="noopener noreferrer">
+                <img src={image.src} alt={image.alt} />
+              </a>
+              <div className={styles.desc}>{image.alt}</div>
+            </div>
+          ))}
         </div>
       </div>
-      <button className={styles.button} onClick={() => navigate("/shop")}>
-        Go Shopping
-      </button>
-    </div>
+    </>
   );
 };
 
